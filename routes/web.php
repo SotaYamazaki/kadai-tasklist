db154ead39080d4018ebd21acdabc0e57f54c4db
souta.yamazaki <souta7408@gmail.com>
2020-06-12 05:29:29 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,7 @@ Route::get('/', 'TasksController@index');
 
 route::resource('tasks', 'TasksController');
 
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
 
